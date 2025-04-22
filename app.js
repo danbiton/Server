c816a7af2632cf5aaedbdf827e5d32cce1273612
danbiton <166488125+danbiton@users.noreply.github.com>
2025-04-23 00:40:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -32,9 +32,7 @@ app.use(
 
     origin: ["http://localhost:5173",
        "http://localhost:5174",
-
-       "https://construction-client-ten.vercel.app/"
-
+       "https://construction-client-ten.vercel.app"
     ]
   })
 );
